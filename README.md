595805f3fdb3e5d16ff3684ae15680e2ab9b292e
  <>
2018-04-10 19:12:40 +0530

    Upload SourceCode

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# HeraCoin
+
+HeraCoin is a PoS-based Cryptocurrency.
+
+# HeraCoin Specification
+
+Coin Name : HeraCoin
+
+Ticker : HER
+
+Coin Type : POS/MN
+
+Algorithm : Scrypt
+
+Block Time : 60
+
+Block Size : 3 MB
+ 
+Maximum Supply: 46,000,000
+
+Minimum Stake Age : 6 Hour
+
+Coin Maturity : 15 Block
+
+RPC Port: 16047
+
+Network Port: 16048
---
+++ src/checkpoints.cpp
@@ -0,0 +1,93 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "txdb.h"
+#include "main.h"
+#include "uint256.h"
+
+
+static const int nCheckpointSpan = 5000;
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+		(0, uint256("0x0000bfa54e1209a4a9ae0179a3c290fad1f1a8ea742271e31f6bc27ae742fa6c"))
+		(1, uint256("0x0f27ff1f57c046fe22e6f93bef113d98403192eac018c94647cbb5beb110dede"))
+		(2, uint256("0x7397b74a1315fdb02e1bcc87588009b9ca68219aea478f4668dce09c0a6cc35b"))
+		(29, uint256("0xc959119bc7e7229a22eb6500bb8680e5b2d8f35bab3dc706788569ce17259f3f"))
+		(62, uint256("0x9b13892094c42e43f06c226a2a625cf23aeb2a9b208a4f013735132aec290490"))
+		(80, uint256("0xcf7b7368cf850b8ca759efe75d7ff25448e040cc39f0005e758974989f429608"))
+		(102, uint256("0x1ccbe7609997a445a3e416892a1171776f29307492a38f17799712906d345974"))
+    ;
+
+    // TestNet has no checkpoints
+    static MapCheckpoints mapCheckpointsTestnet;
+
+    bool CheckHardened(int nHeight, const uint256& hash)
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
+        if (i == checkpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        if (checkpoints.empty())
+            return 0;
+        return checkpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, checkpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+
+    // Automatically select a suitable sync-checkpoint 
+    const CBlockIndex* AutoSelectSyncCheckpoint()
+    {
+        const CBlockIndex *pindex = pindexBest;
+        // Search backward for a block within max span and maturity window
+        while (pindex->pprev && pindex->nHeight + nCheckpointSpan > pindexBest->nHeight)
+            pindex = pindex->pprev;
+        return pindex;
+    }
+
+    // Check against synchronized checkpoint
+    bool CheckSync(int nHeight)
+    {
+        const CBlockIndex* pindexSync = AutoSelectSyncCheckpoint();
+        if (nHeight <= pindexSync->nHeight){
+            return false;
+        }
+        return true;
+    }
+}
